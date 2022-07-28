0509f93fcdbcdd9246ee25b08eaab884dcd90510
emi-sabathier <sabathier.emilie@gmail.com>
2022-07-28 11:48:34 +0200

    git init

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {SafeAreaView, ScrollView, Text} from 'react-native';
+
+const App = () => {
+  return (
+    <SafeAreaView>
+      <ScrollView>
+        <Text>Hello</Text>
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
+
+export default App;
