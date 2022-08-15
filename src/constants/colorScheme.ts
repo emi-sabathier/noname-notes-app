7264f4b580de16e490675d09f603cc4500eceeca
E.Sabathier <47697572+emi-sabathier@users.noreply.github.com>
2022-08-15 11:38:32 +0200

    Revert "Feature/card color retry"

--- src/constants/colorScheme.ts
+++
@@ -1,9 +0,0 @@
-export const colorScheme = {
-    white: '#FFF',
-    black: '#000',
-    yellow: '#ffff00',
-    cyan300: '#4DD0E1',
-    grey300: '#E0E0E0',
-    grey800: '#424242',
-    primaryColor: '#1B1B1B',
-};
--- src/models/NoteModel.ts
+++
@@ -1,9 +0,0 @@
-export type NoteColor = 'white' | 'lightblue' | 'lemonchiffon' | 'orange' | 'lightgreen';
-
-export interface Note {
-    id?: string;
-    title: string;
-    content: string;
-    archive: boolean;
-    noteColor: NoteColor;
-}
