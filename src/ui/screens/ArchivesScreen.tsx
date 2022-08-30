fe2ff8c94595ca212460d06ca234fd6132738907
emi-sabathier <sabathier.emilie@gmail.com>
2022-08-30 12:11:28 +0200

    fix: temporary fix ts note archives

--- src/ui/screens/ArchivesScreen.tsx
+++ src/ui/screens/ArchivesScreen.tsx
@@ -3,6 +3,7 @@ import { UIContainer } from '../shared/UIContainer';
 import { FlatList, StyleSheet, View } from 'react-native';
 import { UINoteCard } from '../components/UINoteCard';
 import { useAppSelector } from '../../store/hooks';
+import { Note } from '../../models/NoteModel';
 
 export const ArchivesScreen = () => {
     const notesList = useAppSelector(state => state.notes);
@@ -17,7 +18,7 @@ export const ArchivesScreen = () => {
                             numColumns={2}
                             data={archivesList}
                             keyExtractor={(note, i) => i.toString()}
-                            renderItem={({ item }) => <UINoteCard note={item} key={item.id} />}
+                            renderItem={({ item }) => <UINoteCard note={item as Note} key={item.id} />}
                         />
                     </>
                 ) : null}
